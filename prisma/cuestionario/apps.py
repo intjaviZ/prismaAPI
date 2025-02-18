322ff4782de330dfe079297a2a903f7d74487893
javi <elbuenjavizarate@gmail.com>
2025-02-18 00:33:32 -0600

    apps listas2

--- prisma/cuestionario/apps.py
+++ prisma/cuestionario/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CuestionarioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cuestionario'
+    name = 'prisma.cuestionario'
