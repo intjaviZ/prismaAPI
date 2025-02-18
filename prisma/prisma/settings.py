b9d277b8acae6abf35553fafb23aea9705816e96
javi <elbuenjavizarate@gmail.com>
2025-02-18 01:35:52 -0600

    coros

--- prisma/prisma/settings.py
+++ prisma/prisma/settings.py
@@ -17,9 +17,10 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5500"
-]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "http://127.0.0.1:5500"
+# ]
 
 
 # Application definition
